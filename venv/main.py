0115b57dbbd91095efbda8530efe2d8e49bd12c1
Joao Guilherme <jgriquena@gmail.com>
2024-01-23 21:29:00 -0300

    .

--- venv/main.py
+++ venv/main.py
@@ -1,51 +1,66 @@
 import sqlalchemy
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, Session
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, create_engine, inspect
-from sqlalchemy import Integer
-from sqlalchemy import String
-from sqlalchemy import ForeignKey
+from sqlalchemy import Integer, String, ForeignKey
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users_account'
-    # atributos
     id = Column(Integer, primary_key=True)
     name = Column(String)
     fullname = Column(String)
     
     addresses = relationship(
-        "Addresses", back_populates="user", cascade="all"
+        "Address", back_populates="user", cascade="all"
     )
     
     def __repr__(self):
         return f"User(id={self.id}, name={self.name}, fullname={self.fullname})"
-    
-class Addresses(Base):
+
+class Address(Base):
     __tablename__ = "addresses"
     id = Column(Integer, primary_key=True)
     email_address = Column(String(30), nullable=False)
-    user_id = Column(Integer, ForeignKey("users_account.id"), nullable=False)  # Corrigido o nome da tabela aqui
+    user_id = Column(Integer, ForeignKey("users_account.id"), nullable=False)
     
     user = relationship("User", back_populates="addresses")
     
     def __repr__(self):
-        return f"Address(id={self.id}, email={self.email_address})"
+        return f"Address(id={self.id}, email_address={self.email_address})"
 
-
-print(User.__tablename__)
-print(Addresses.__table__)
-
-#conexão com o banco de dados
+# Conexão com o banco de dados
 engine = create_engine("sqlite://")
 
-#criando as classes como tabelas no banco de dados
+# Criando as classes como tabelas no banco de dados
 Base.metadata.create_all(engine)
 
-# investiga o esquema do banco de dados
-inspetor_engine = inspect(engine)
-print(inspetor_engine.has_table("user_account"))
+# Investigando o esquema do banco de dados
+inspector_engine = inspect(engine)
+print(inspector_engine.has_table("users_account"))
+print(inspector_engine.get_table_names())
+print(inspector_engine.default_schema_name)
 
-print(inspetor_engine.get_table_names())
-print(inspetor_engine.default_schema_name)
+with Session(engine) as session:
+    juliana = User(
+        name='juliana',
+        fullname='Juliana Mascarenhas',
+        addresses=[Address(email_address='julianam@email.com')]
+    )
+    
+    sandy = User(
+        name='sandy',
+        fullname='Sandy Cardoso',
+        addresses=[Address(email_address='sandy@email.br'),
+                   Address(email_address='sandy@email.org')]
+    )
+    
+    patrick = User(
+        name='patrick',
+        fullname='Patrick Cardoso'
+    )
+    
+    # Enviando para o BD (persistência de dados)
+    session.add_all([juliana, sandy, patrick])
+    session.commit()
